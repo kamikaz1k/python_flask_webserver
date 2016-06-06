96201f22ad3cff0df1d8789c3af20b76ccb55600
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2016-06-06 03:07:11 -0400

    Moved connection/cursor init to the signUp function, and added a connection/cursor close at the end of the function. Wrapped db calls in try/exception/finally blocks. Reimplemented form field empty check

--- app.py
+++ app.py
@@ -21,12 +21,6 @@ app.config['MYSQL_DATABASE_DB'] = 'BucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-# Connect to DB
-conn = mysql.connect()
-
-# Retrieve DB Cursor
-cursor = conn.cursor()
-
 # Routing Definitions
 @app.route("/")
 def main():
@@ -43,33 +37,46 @@ def signUp():
     _email = request.form['inputEmail']
     _password = request.form['inputPassword']
 
-    # Generate password hash
-    _hashed_password = generate_password_hash(_password)
-    print "_hashed_password: ", _hashed_password
+    # Check that the forms were filled out
+    if _name and _email and _password:
 
-    cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+        # Generate password hash
+        _hashed_password = generate_password_hash(_password)
+        print "_hashed_password: ", _hashed_password
 
-    data = cursor.fetchall()
-     
-    if len(data) is 0:
-        conn.commit()
-        return json.dumps({'message':'User created successfully !'})
+        # Wrap db calls in try/except/finally
+        try:
+            # Connect to DB
+            conn = mysql.connect()
+
+            # Retrieve DB Cursor
+            cursor = conn.cursor()
+            cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+
+            data = cursor.fetchall()
+
+             
+            if len(data) is 0:
+                # Commit changes to db
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                print "Username already exists? - ", data
+                return json.dumps({'error':str(data[0])})
+
+        # Catch any exceptions
+        except Exception as e:
+            return json.dumps({'error':str(e)})
+
+        # Finally close cursor & connection so that next 
+        # transaction can take place separately
+        finally:
+            cursor.close()
+            conn.close()
+
+    # IF the signup form fields were not populated
     else:
-        return json.dumps({'error':str(data[0])})
-
-    # # validate the received values
-    # if _name and _email and _password:
-    #     return json.dumps({'html':'<span>All fields good !!</span>'})
-    # else:
-    #     return json.dumps({'html':'<span>Enter the required fields</span>'})
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 if __name__ == "__main__":
 	app.run()
-
-
-
-# CREATE TABLE `BucketList`.`tbl_user` (`user_id` BIGINT NOT NULL AUTO_INCREMENT, `user_name` VARCHAR(45) NULL, `user_username` VARCHAR(45) NULL, `user_password` VARCHAR(45) NULL, PRIMARY KEY (`user_id`));
-
-# DELIMITER $$ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createUser`( IN p_name VARCHAR(20), IN p_username VARCHAR(20), IN p_password VARCHAR(20) ) BEGIN if ( select exists (select 1 from tbl_user where user_username = p_username) ) THEN select 'Username Exists !!'; ELSE insert into tbl_user ( user_name, user_username, user_password ) values ( p_name, p_username, p_password ); END IF; END$$ DELIMITER;
-
-# select * from BucketList.information_schema.routines where routine_type = 'PROCEDURE';
